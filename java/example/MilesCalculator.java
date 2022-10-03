8a9008b4f2223658424d3b913efd0255faa11e09
Java Instructor <info@openlearn.co.za>
2022-10-03 21:58:25 +0200

    Miles and Printer files

---
+++ java/example/MilesCalculator.java
@@ -0,0 +1,5 @@
+public class MilesCalculator{
+	public static double calculate(String kms){
+		return 0;
+	}
+}
---
+++ java/example/Printer.java
@@ -0,0 +1,5 @@
+public class Printer{
+	public static void print(String name,String surname, String dob,String kms,double miles,int age){
+
+	}
+}
